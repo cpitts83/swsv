6ba744d6d2a94834b707be31dddc1a83fe654d8f
Cleveland Pitts <sotechnicool@Clevelands-MacBook-Pro.local>
2023-06-27 21:20:06 -0500

    added logo to Header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,17 @@
 <nav class="bg-gray-200 p-6 rounded-xl shadow-neumorphism">
     <ul class="flex space-x-4">
-        <li><a href="/" class="text-gray-800 hover:text-gray-600">Home</a></li>
+        <li><a href="/" class="text-gray-800 hover:text-gray-600"><img src="./logo.png" alt="" srcset=""></a></li>
         <li><a href="/about" class="text-gray-800 hover:text-gray-600">About</a></li>
         <li><a href="/blog" class="text-gray-800 hover:text-gray-600">Contact</a></li>
     </ul>
 </nav>
+
+<style>
+    .shadow-neumorphism {
+        box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff;
+    }
+    img {
+        width: 50px;
+        height: 50px;
+    }
+</style>
